2c076491f422d7e61d2781f4ea47ac789fd40950
Robert Kenny <kenoir@gmail.com>
2012-06-26 13:28:58 +0100

    added amd compatibility

--- public/javascripts/drgbl.js
+++ public/javascripts/drgbl.js
@@ -1,157 +1,179 @@
-var Draggable = function(element, opts) {
-    var draggable = this;
+;(function(exports) {
 
-    draggable.events = this.deviceEvents();
-    draggable.axis = undefined;
-    draggable.callback = {};
-    draggable.bound = function(m){return true;};
-    draggable.options(opts);
+	function Draggable(element, opts) {
+		var draggable = this;
 
-    element.draggableInstance = draggable;
+		draggable.events = this.deviceEvents();
+		draggable.axis = undefined;
+		draggable.callback = {};
+		draggable.bound = function(m){return true;};
+		draggable.options(opts);
 
-    Draggable.addListener(element,draggable.events.dragstart);
-}
-Draggable.prototype.options = function(opts){
-    var draggable = this;
-    if (opts) {
-        if (opts.axis == "x" || opts.axis == "y") {
-            draggable.axis = opts.axis;
-        }
-	if(opts.callback){
-	    draggable.callback = opts.callback;
+		element.draggableInstance = draggable;
+
+		Draggable.addListener(element,draggable.events.dragstart);
 	}
-	if(opts.bound instanceof Function){
-	    draggable.bound = opts.bound;
+
+	Draggable.prototype.options = function(opts){
+		var draggable = this;
+		if (opts) {
+			if (opts.axis == "x" || opts.axis == "y") {
+				draggable.axis = opts.axis;
+			}
+			if(opts.callback){
+				draggable.callback = opts.callback;
+			}
+			if(opts.bound instanceof Function){
+				draggable.bound = opts.bound;
+			}
+		}
 	}
-    }
-}
-Draggable.prototype.after = function(e,name){
-    var target = Draggable.dragging || Draggable.eventTarget(e);
-    var draggable = target.draggableInstance; 
+	Draggable.prototype.after = function(e,name){
+		var target = Draggable.dragging || Draggable.eventTarget(e);
+		var draggable = target.draggableInstance; 
 
-    if(draggable && draggable.callback[name] instanceof Function){
-      draggable.callback[name].call(target);
-    }
-}
-Draggable.prototype.deviceEvents = function(){
-    var isTouchDevice = !!('ontouchstart' in window) ? 1 : 0;
+		if(draggable && draggable.callback[name] instanceof Function){
+			draggable.callback[name].call(target);
+		}
+	}
+	Draggable.prototype.deviceEvents = function(){
+		var isTouchDevice = !!('ontouchstart' in window) ? 1 : 0;
 
-    var touchEvents = { 
-	    dragstart:'touchstart',
-	    dragging:'touchmove',
-	    dragend:'touchend'
-    }
-    var mouseEvents = {
-	    dragstart:'mousedown',
-	    dragging:'mousemove',
-	    dragend:'mouseup'
-    }
+		var touchEvents = { 
+			dragstart:'touchstart',
+			dragging:'touchmove',
+			dragend:'touchend'
+		}
+		var mouseEvents = {
+			dragstart:'mousedown',
+			dragging:'mousemove',
+			dragend:'mouseup'
+		}
 
-    return ( isTouchDevice ? touchEvents : mouseEvents );
-}
-Draggable.prototype.dragstart = function(e) {
-    var draggable = this.draggableInstance;
+		return ( isTouchDevice ? touchEvents : mouseEvents );
+	}
+	Draggable.prototype.dragstart = function(e) {
+		var draggable = this.draggableInstance;
 
-    this.style.position = 'relative';
-    Draggable.addListener(document,draggable.events.dragging);
-    Draggable.addListener(document,draggable.events.dragend);
+		this.style.position = 'relative';
+		Draggable.addListener(document,draggable.events.dragging);
+		Draggable.addListener(document,draggable.events.dragend);
 
-    var startingPosition = Draggable.dragPosition(e);
-    var currentTargetPosition = Draggable.targetPosition(e);
+		var startingPosition = Draggable.dragPosition(e);
+		var currentTargetPosition = Draggable.targetPosition(e);
 
-    draggable.offset = {
-        x: currentTargetPosition.x - startingPosition.x,
-        y: currentTargetPosition.y - startingPosition.y,
-    }
+		draggable.offset = {
+			x: currentTargetPosition.x - startingPosition.x,
+			y: currentTargetPosition.y - startingPosition.y,
+		}
 
-    Draggable.dragging = this; 
-}
-Draggable.prototype.dragging = function(e) {
-    var draggable = Draggable.dragging.draggableInstance;
-    var inPixels = function(n){
-	return n + "px";
-    }
+		Draggable.dragging = this; 
+	}
+	Draggable.prototype.dragging = function(e) {
+		var draggable = Draggable.dragging.draggableInstance;
+		var inPixels = function(n){
+			return n + "px";
+		}
 
-    var currentMousePosition = Draggable.dragPosition(e);
-    var movePosition = {
-	    x: currentMousePosition.x + draggable.offset.x,
-	    y: currentMousePosition.y + draggable.offset.y
-    }
+		var currentMousePosition = Draggable.dragPosition(e);
+		var movePosition = {
+			x: currentMousePosition.x + draggable.offset.x,
+			y: currentMousePosition.y + draggable.offset.y
+		}
 
-    if(draggable.bound(movePosition)){
+		if(draggable.bound(movePosition)){
 
-	  if (draggable.axis == 'x' || draggable.axis == undefined) {
-	    Draggable.dragging.style.left = inPixels(movePosition.x);
-          }
-	  if (draggable.axis == 'y' || draggable.axis == undefined) {
-	    Draggable.dragging.style.top = inPixels(movePosition.y);
-          }
+			if (draggable.axis == 'x' || draggable.axis == undefined) {
+				Draggable.dragging.style.left = inPixels(movePosition.x);
+			}
+			if (draggable.axis == 'y' || draggable.axis == undefined) {
+				Draggable.dragging.style.top = inPixels(movePosition.y);
+			}
 
-    }
-}
-Draggable.prototype.dragend = function(e) {
-    var draggable = this.draggableInstance;
-    Draggable.removeListener(document, draggable.events.dragging);
-    Draggable.removeListener(document, draggable.events.dragend);
-    Draggable.dragging = undefined; 
-}
+		}
+	}
+	Draggable.prototype.dragend = function(e) {
+		var draggable = this.draggableInstance;
+		Draggable.removeListener(document, draggable.events.dragging);
+		Draggable.removeListener(document, draggable.events.dragend);
+		Draggable.dragging = undefined; 
+	}
 
-Draggable.targetPosition = function(e) {
-    var extractInt = function(value) {
-        var n = parseInt(value);
-        return n == null || isNaN(n) ? 0 : n;
-    }
-    var target = Draggable.eventTarget(e);
+	Draggable.targetPosition = function(e) {
+		var extractInt = function(value) {
+			var n = parseInt(value);
+			return n == null || isNaN(n) ? 0 : n;
+		}
+		var target = Draggable.eventTarget(e);
 
-    return {
-        x: extractInt(target.style.left),
-        y: extractInt(target.style.top),
-    }
-}
-Draggable.eventTarget = function(e) {
-    return e.target || e.srcElement;
-}
-Draggable.handleEvent = function(e){
-    if (!e) e = window.event;
-    var target = Draggable.eventTarget(e);
-    var draggable = target.draggableInstance || Draggable.dragging.draggableInstance;
+		return {
+			x: extractInt(target.style.left),
+			y: extractInt(target.style.top),
+		}
+	}
+	Draggable.eventTarget = function(e) {
+		var target = e.target || e.srcElement; 
+		var localTarget = target;
 
-    for(var eventName in draggable.events){
-      if(draggable.events[eventName] == e.type){
-	draggable[eventName].call(target,e);
-	draggable.after(e,eventName);
-      }
-    }
-}
-Draggable.dragPosition = function(e) {
-    var posx = 0;
-    var posy = 0;
-    if (!e) var e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY) {
-        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-    return {
-        x: posx,
-        y: posy
-    };
-}
-Draggable.addListener = function(target,eventName){
-    if(target.addEventListener){
-      target.addEventListener(eventName,Draggable.handleEvent,false);
-    } else if(target.attachEvent){
-      target.attachEvent('on' + eventName,Draggable.handleEvent);
-    } 
-}
-Draggable.removeListener = function(target,eventName){
-    if(target.removeEventListener){ 
-      target.removeEventListener(eventName, Draggable.handleEvent, false);
-    } else if(target.detachEvent){
-      target.detachEvent('on' + eventName,Draggable.handleEvent);
-    }
-}
+		while(localTarget.parentNode && 
+		      localTarget.draggableInstance == undefined){
+		      
+	          localTarget = localTarget.parentNode	      
+		}
+		if(localTarget.draggableInstance) target = localTarget;
 
+		return target; 
+	}
+	Draggable.handleEvent = function(e){
+		if (!e) e = window.event;
+		var target = Draggable.eventTarget(e);
+		var draggable = target.draggableInstance || Draggable.dragging.draggableInstance;
+
+		for(var eventName in draggable.events){
+			if(draggable.events[eventName] == e.type){
+				draggable[eventName].call(target,e);
+				draggable.after(e,eventName);
+			}
+		}
+	}
+	Draggable.dragPosition = function(e) {
+		var posx = 0;
+		var posy = 0;
+		if (!e) var e = window.event;
+		if (e.pageX || e.pageY) {
+			posx = e.pageX;
+			posy = e.pageY;
+		}
+		else if (e.clientX || e.clientY) {
+			posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		return {
+			x: posx,
+			y: posy
+			};
+	}
+	Draggable.addListener = function(target,eventName){
+		if(target.addEventListener){
+			target.addEventListener(eventName,Draggable.handleEvent,false);
+		} else if(target.attachEvent){
+			target.attachEvent('on' + eventName,Draggable.handleEvent);
+		} 
+	}
+	Draggable.removeListener = function(target,eventName){
+		if(target.removeEventListener){ 
+			target.removeEventListener(eventName, Draggable.handleEvent, false);
+		} else if(target.detachEvent){
+			target.detachEvent('on' + eventName,Draggable.handleEvent);
+		}
+	}
+
+	if(typeof define === 'function' && define.amd) {
+		define(function() {
+			return Draggable;
+		});
+	}
+
+	exports.Draggable = Draggable;
+
+}(this));
